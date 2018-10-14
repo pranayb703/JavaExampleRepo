bf0b997e321fe0ec507ac1c5428a908105eb3af3
pranay.bhandarkar <pranay.bhandarkar@servicenow.com>
2018-10-13 19:06:05 -0700

    First commit LL

---
+++ LinkedListExample.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+// @Example Implement Reverse of Linked List
+
+//@Example Arrange LinkedList in Odd number followed by Even.
+
+//@author Pranay B
+
+public class LinkedListExample {
+	
+	static void reverse(){
+		System.out.println("REVERSE");
+	}
+	
+	public static void main(String args[]){
+		System.out.println("Hello World");
+		reverse();
+	}
+}
